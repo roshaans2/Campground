0222c679ff1be2c629dbfed49b87b2fd61cfcf62
roshaans2 <roshaansathya@gmail.com>
2023-12-27 10:10:44 +0530

    Authentication

--- app.js
+++ app.js
@@ -9,10 +9,14 @@ const catchAsync = require("./utils/catchAsync")
 const ExpressError = require("./utils/ExpressError")
 const Joi = require("joi")
 const {campgroundSchema,reviewSchema} = require("./schema")
-const campgrounds = require("./routes/campgrounds")
-const reviews = require("./routes/reviews")
+const campgroundRoutes = require("./routes/campgrounds")
+const reviewRoutes = require("./routes/reviews")
+const userRoutes = require("./routes/users")
 const session = require("express-session")
 const flash = require("connect-flash")
+const passport = require("passport")
+const LocalStrategy = require("passport-local")
+const User = require("./models/user")
 
 const app = express()
 
@@ -40,15 +44,23 @@ const sessionConfig = {
 app.use(session(sessionConfig))
 app.use(flash())
 
+app.use(passport.initialize())
+app.use(passport.session())
+passport.use(new LocalStrategy(User.authenticate()))
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+
 app.use((req,res,next)=>{
+    res.locals.currentUser = req.user
     res.locals.success = req.flash('success')
     res.locals.error = req.flash('error')
     next()
 })
 
+app.use("/",userRoutes)
+app.use("/campgrounds",campgroundRoutes)
+app.use("/campgrounds/:id/reviews",reviewRoutes)
 
-app.use("/campgrounds",campgrounds)
-app.use("/campgrounds/:id/reviews",reviews)
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,6 +3,7 @@ const Campground = require("../models/campground")
 const catchAsync = require("../utils/catchAsync")
 const ExpressError = require("../utils/ExpressError")
 const {campgroundSchema} = require("../schema")
+const {isLoggedIn} = require("../middleware")
 const router = express.Router()
 
 
@@ -19,7 +20,7 @@ const validateCampground = (req,res,next) => {
 }
 
 
-router.get("/new",async(req,res)=>{
+router.get("/new",isLoggedIn,async(req,res)=>{
     res.render("campgrounds/new")
 })
 
@@ -38,27 +39,27 @@ router.get("/:id",catchAsync(async(req,res)=>{
     res.render("campgrounds/show",{campground})
 }))
 
-router.get("/:id/edit",catchAsync(async(req,res)=>{
+router.get("/:id/edit",isLoggedIn,catchAsync(async(req,res)=>{
     const {id} = req.params
     const campground = await Campground.findById(id)
     res.render("campgrounds/edit",{campground})
 }))
 
-router.post("/",validateCampground,catchAsync(async(req,res,next)=>{
+router.post("/",isLoggedIn,validateCampground,catchAsync(async(req,res,next)=>{
     const campground = Campground(req.body.campground)
     await campground.save()
     req.flash("success","Successfully made a new Campground!")
     res.redirect(`/campgrounds/${campground.id}`)
 }))
 
-router.put("/:id",validateCampground,catchAsync(async(req,res)=>{
+router.put("/:id",isLoggedIn,validateCampground,catchAsync(async(req,res)=>{
     const {id} = req.params
     const campground = await Campground.findByIdAndUpdate(id,{...req.body.campground})
     req.flash("success","Successfully updated Campground!")
     res.redirect(`/campgrounds/${campground.id}`)
 }))
 
-router.delete("/:id",catchAsync(async(req,res)=>{
+router.delete("/:id",isLoggedIn,catchAsync(async(req,res)=>{
     const {id} = req.params
     await Campground.findByIdAndDelete(id)
     res.redirect("/campgrounds")
